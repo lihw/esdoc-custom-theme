9955d828392c0fb850f73b668acf1bc35199c37f
Gregor Adams <greg@pixelass.com>
2018-02-12 01:13:04 +0100

    fix(syntax-highlighter): better mode handling
    modes were not recognized correctly. Adds easier and safer handling

--- src/components/syntax-highlighter/highlight.js
+++ src/components/syntax-highlighter/highlight.js
@@ -1,6 +1,24 @@
 import {pathname} from '../url'
 import {simpleMode, fullMode} from '../codemirror'
 
+const JAVASCRIPT = 'javascript'
+const JSX = 'jsx'
+const TYPESCRIPT = 'text/typescript'
+const JSON_LD = 'application/ld+json'
+const SHELL = 'shell'
+
+const modes = {
+  bash: SHELL,
+  shell: SHELL,
+  javascript: JAVASCRIPT,
+  js: JAVASCRIPT,
+  jsx: JSX,
+  typescript: TYPESCRIPT,
+  ts: TYPESCRIPT,
+  tsx: TYPESCRIPT,
+  json: JSON_LD
+}
+
 const highlight = blocks => {
   return blocks.map(block => {
     const {innerText: value} = block
@@ -15,15 +33,16 @@ const highlight = blocks => {
     const isTSXFile = pathname.match('.tsx.html')
     const isFile = isJSFile || isTSFile || isJSXFile || isTSXFile
     let mode = lang.replace(/lang(uage)?-/, '')
+
     if (isJSFile || isJSXFile) {
       mode = 'jsx'
     }
     if (isTSFile || isTSXFile) {
-      mode = 'tsx'
+      mode = 'ts'
     }
     return isFile
-      ? fullMode(value, mode, block)
-      : simpleMode(value, mode, block)
+      ? fullMode(value, modes[mode], block)
+      : simpleMode(value, modes[mode], block)
   })
 }
 

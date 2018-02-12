db8fcd0d09b49cbc58743d7d17044cc0a9c9be8e
Gregor Adams <greg@pixelass.com>
2018-02-12 07:58:38 +0100

    docs(syntax-highlighter): add docs (for demo)
    mainly added this as a demo

--- src/components/syntax-highlighter/normalize-lines.js
+++ src/components/syntax-highlighter/normalize-lines.js
@@ -1,3 +1,15 @@
+/**
+ * Normalizes lines
+ *
+ * @param  {Array} lines
+ *   The lines
+ * @return {Array}
+ *   sorted and 0 based index
+ *
+ * @example
+ * const lines = normalizeLines(['1', '12', '3', '5'])
+ * // => [0, 2, 4, 11]
+ */
 const normalizeLines = lines =>
   lines
     .split(',')

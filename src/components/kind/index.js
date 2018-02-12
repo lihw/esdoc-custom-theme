de449d3fa9dceef422201c3e1eb17e3a588aef31
Gregor Adams <greg@pixelass.com>
2018-02-12 08:03:21 +0100

    fix(kind): add missing kinds
    also adds a typedef to test colors

--- src/components/kind/index.js
+++ src/components/kind/index.js
@@ -1,2 +1,22 @@
 // CSS file generation
 import './kind.scss'
+
+/**
+ * @typedef TestTypeDef1
+ *
+ * This is just a test for typedef
+ * @see {TestTypeDef2}
+ * @type {function}
+ * @param {!Object} foo Just an object
+ * @param {?String} bar This is a string
+ * @param {*} [baz=''hello] This anything
+ */
+
+ /**
+ * @typedef TestTypeDef2
+ *
+ * This is just a test for typedef
+ * @type Object
+ * @property {String} foo About foo
+ */
+

5b1e0197f2223de54750d0cc064c98b60c5a97f6
Gregor Adams <greg@pixelass.com>
2018-02-12 08:02:09 +0100

    feat(base): activate hash-targets
    mark hash-targets to better find them on the page

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,2 +1,28 @@
+import {hash} from '../url'
+import {$, $$} from '../global-elements'
+
 // CSS file generation
 import './base.scss'
+
+
+const markHashTarget = hash => {
+  if (hash) {
+    const target = $(hash)
+    if (target) {
+      target.classList.add('active-target')
+    }
+  }
+}
+
+markHashTarget(hash)
+
+window.addEventListener('hashchange', e => {
+  $$('.active-target').forEach(target => {
+    target.classList.remove('active-target')
+  })
+  const {newURL} = e
+  const [,hash] = newURL.split('#')
+  if (hash) {
+    markHashTarget(`#${hash}`)
+  }
+})
